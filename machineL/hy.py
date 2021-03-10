8a5393fef32b6af1d7364b27a7bc76ccdca37cdf
RinozaJiffry <66359174+RinozaJiffry@users.noreply.github.com>
2021-03-11 02:26:04 +0530

    updated sentiment analysis

--- machineL/hy.py
+++ machineL/hy.py
@@ -1,2 +1,15 @@
-print("HelloWorld")
-print(5)
+import pandas as pd
+import numpy as np
+import re
+import nltk
+from sklearn.datasets import load_files
+nltk.download('stopwords')
+import pickle
+from nltk.corpus import stopwords
+
+# read data
+reviews = pd.read_csv("reviews.csv")
+reviews
+
+reviews.columns
+reviews.drop([ 'Unnamed: 3', 'Unnamed: 4','Unnamed: 5','Unnamed: 6'],axis = 1)
