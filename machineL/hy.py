23a857ec0f9ee017b4f5b885862b8949448d8a5f
RinozaJiffry <66359174+RinozaJiffry@users.noreply.github.com>
2021-03-23 11:33:09 +0530

    Updated hy.py
    updated sentiment analysis

--- machineL/hy.py
+++ machineL/hy.py
@@ -1,15 +1,19 @@
 import pandas as pd
 import numpy as np
-import re
-import nltk
-from sklearn.datasets import load_files
-nltk.download('stopwords')
+import text_processor as tp
 import pickle
-from nltk.corpus import stopwords
+
 
 # read data
 reviews = pd.read_csv("reviews.csv")
 reviews
 
-reviews.columns
-reviews.drop([ 'Unnamed: 3', 'Unnamed: 4','Unnamed: 5','Unnamed: 6'],axis = 1)
+def process_df(reviews):
+    reviews1 = reviews[['Text','Score']]
+    reviews1_filter = reviews1[reviews1['Score'] != 3]
+    sentiment_dict = {1:0, 2:0, 4:1, 5:1} 
+    reviews1_filter['sentiment Analysis'] = reviews1_filter['Score'].map(sentiment_dict)
+    return reviews1_filter
+  
+ reviews1 = process_df(reviews)
+ reviews1.head() 
