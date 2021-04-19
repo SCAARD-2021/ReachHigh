a6e7e49f4ca149abb35bbdb5735e0893b876c523
ShakirAMD <65905459+ShakirAMD@users.noreply.github.com>
2021-04-19 20:24:06 +0530

    Blog page updated

---
+++ client/Version 2/src/components/Blog.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import { callAppLink } from "../Service/EndPoints";
+
+import "./Blog.css";
+import { Link } from "react-router-dom";
+import Confirmation from "./Confirmation";
+
+function Blog() {
+  
+  
+  
+
+
+  
+  return (
+    <div >
+      <div class="grid-container"></div>
+      <a href="#" class="classname">CSS</a>
+      <p>dxfzx fzx zxf zf zsd </p>
+      <p>sda sa da</p>
+      
+    </div>
+  );
+};
+
+export default Blog;
