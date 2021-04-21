90c24ce7dd7de0a77723426e603978fcbb2da659
Abdullah-hunais <abdullah.2019688@iit.ac.lk>
2021-04-21 16:36:54 +0530

    updated Confirmation

--- client/Version 2/src/components/AboutUs.js
+++ client/Version 2/src/components/AboutUs.js
@@ -5,21 +5,28 @@ import img2 from "../Images/caushi.jpg";
 import img3 from "../Images/delojan.jpg";
 import img4 from "../Images/Shakir.jpg";
 import img5 from "../Images/rinoza.jpg";
-import img6 from "../Images/Abdullah.jpg";
+import img6 from "../Images/abdullah.png";
+import group from "../Images/group.jpg";
+import Typewriter from "typewriter-effect";
 
 function AboutUs() {
   return (
     <div className="about-us-main">
       <div className="about-us-first">
         <div className="about-us-text1">
-          <h1 id="aboutUsH1">Hello World this is S C A A R D</h1>
+          <Typewriter
+            className="aboutUsH1"
+            onInit={(Typewriter) => {
+              Typewriter.typeString("Hello World this is S C A A R D").start();
+            }}
+          />
           <h2 id="aboutUsH2">S C A A R D is everywhere..</h2>
           <h3 id="aboutUsH3">
             Learn more about our story and the hard-works <br /> people behind
             the phone is developers
           </h3>
         </div>
-        <div className="ourImage"></div>
+        <img src={group} />
       </div>
       <div className="about-us-second">
         <div className="about-us-text2">
@@ -39,37 +46,37 @@ function AboutUs() {
         <div className="developer">
           <img src={img1} />
           <h3 id="name">Adrian Anchelo</h3>
-          <h6 id="workTitle">back-end Developer</h6>
+          <h6 id="workTitle">Project Leader</h6>
         </div>
 
         <div className="developer">
           <img src={img2} />
-          <h3 id="name">Krishnamoorthy Caucidheesan </h3>
-          <h6 id="workTitle">ML - Developer</h6>
+          <h3 id="name"> Caucidheesan </h3>
+          <h6 id="workTitle"> Data Scientist </h6>
         </div>
 
         <div className="developer">
           <img src={img3} />
           <h3 id="name">George Delojan</h3>
-          <h6 id="workTitle">back-end developer</h6>
+          <h6 id="workTitle">AWS Tech</h6>
         </div>
 
         <div className="developer">
           <img src={img4} />
           <h3 id="name">Shakir Ahamed</h3>
-          <h6 id="workTitle">Front-end developer</h6>
+          <h6 id="workTitle">Front-End Developer</h6>
         </div>
 
         <div className="developer">
           <img src={img5} />
           <h3 id="name">Rinoza Jiffry</h3>
-          <h6 id="workTitle">ML - developer</h6>
+          <h6 id="workTitle"> Data Scientist </h6>
         </div>
 
         <div className="developer">
           <img src={img6} />
           <h3 id="name">Abdullah</h3>
-          <h6 id="workTitle">Front-end developer</h6>
+          <h6 id="workTitle">Front-End Developer</h6>
         </div>
       </div>
     </div>
